bd575ceeeba1e17d12158c92583b480cd806445c
DonghaonQin <dillion0504@gmail.com>
2024-10-18 20:49:49 +1100

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/<repository-name>/'
+      ? '/https://github.com/DonghaoQin/NoMash-Library.git/'
       : '/'
   }
