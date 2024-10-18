17b60538c2203b9127f40214f304db401de93d63
DonghaonQin <dillion0504@gmail.com>
2024-10-18 22:24:45 +1100

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/5032-efolio12/', 
   plugins: [
     vue(),
     vueDevTools(),
